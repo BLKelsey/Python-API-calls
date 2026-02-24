3652143cb42f2f382b2363efe408059c5c0777d7
BLKelsey <blkelsey@gmail.com>
2026-02-24 16:00:43 -0500

    leveraging API fixtures

--- test_using_params.py
+++ test_using_params.py
@@ -1,7 +1,8 @@
 import requests   
-import pytest     # Import pytest (not strictly needed here unless using markers such as @pytest.mark.smoke, skip, etc.)
 
-
+# ------------------
+# RestCountries API
+# ------------------
 def test_get_name_countries(restcountries_base_url, endpoint_url):
     # Combine base URL + path (e.g. https://restcountries.com/v3.1 + /name/turkey)
     url = f"{restcountries_base_url}{endpoint_url}"
@@ -50,6 +51,9 @@ def test_languages(restcountries_base_url, endpoint_url, languages):
     data = response.json()
     print(f"Languages: {data[0]['languages']}")
     
+ #------------------
+ # Exchangerate API
+ #------------------    
 def test_exchangerate(exchangerate_base_url, exchangerate_endpoint_url):
     
     url = f"{exchangerate_base_url}{exchangerate_endpoint_url}"
@@ -58,7 +62,39 @@ def test_exchangerate(exchangerate_base_url, exchangerate_endpoint_url):
     data = response.json()
     usd_to_try = data["rates"]["TRY"]
     print(f"USD → TRY rate: {usd_to_try}")
-
     
+#-------------
+# Weather API
+# ------------      
+def test_weather_base_url(test_weather_base_url):
+    assert "weatherbit.io" in test_weather_base_url
+
+def test_weather(test_weather_base_url, city, country, units, test_key):
+
+    params = {
+        "city": city,
+        "country": country,
+        "units": units,
+        "key": test_key
+    }
+
+    response = requests.get(test_weather_base_url, params=params)
+    assert response.status_code == 200, \
+        f"Expected 200, got {response.status_code}"
+        
+    data = response.json()
+    
+    assert "data" in data, "Response missing 'data' field"
+    assert isinstance(data["data"], list), "'data' should be a list"
+    assert len(data["data"]) > 0, "'data' list is empty"
+
+    # "data" is a list of weather results.
+    # take the first result (index 0) and extract its "temp" value.
+    temp = data["data"][0]["temp"]
+    
+    assert isinstance(temp, (int, float)), \
+     f"Temperature should be numeric, got {type(temp)}"
+
+    print(f"Temperature in {city}: {temp} F")
 
     
